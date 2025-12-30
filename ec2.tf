dffe66d9a5fa2642e7fc9d54fc264a6e7138b448
SrikanthErgula <srikanthreddybtech17@gmail.com>
2025-12-30 21:51:31 +0530

    terraform-module

---
+++ ec2.tf
@@ -0,0 +1,12 @@
+resource "aws_instance" "this" {
+  ami           = var.ami_id  # manadatory
+  instance_type = var.instace_moduletype  # manadatory
+  vpc_security_group_ids = var.sg_ids # manadatory
+  tags = var.tags # Optional
+} # so ikkada instances or server create avuthai based on above code 
+
+# so keys ni changes chyelemu so vati ki vunna values change chetse we can get o/p as 
+# per the changes..check
+
+# General ga use chestunnnaru so we need use "this" syntax not a mandatory just for 
+# understand purpose
---
+++ variable.tf
@@ -0,0 +1,28 @@
+variable "ami_id" {
+  type = string # list of ami-id lu iste error vastundhi bcz here value only string
+  default = "" # ikkada values ichhina use ki chance vunttadhi for edit... so leave it
+  description = "this is the ami used to create EC2 server"
+  # so ila empty ga pedithe values update chayli must be provide
+}
+
+variable "instace_moduletype" {
+type = string
+# so ila empty ga pedithe values update chayli must be provide
+description = "Instance type is used for creating EC2 instance or server"
+validation {
+    condition     = contains(["t3.micro", "t3.small", "t3.medium"], var.instace_moduletype)
+    error_message = "Please select either t3 micro or small or medium"
+  }
+# see in notes
+}
+
+variable "sg_ids" {
+  type = list
+  # so ila empty ga pedithe values update chayli must be provide
+}
+
+# optional
+variable "tags" {
+  type = map
+  default = {} # ila pedithe idhi optinal ani meaning
+}
