e096386baa27d03290e6aafebc117b65daac08ef
SrikanthErgula <srikanthreddybtech17@gmail.com>
2026-07-13 09:45:29 +0530

    modified_code

--- ec2.tf
+++ ec2.tf
@@ -1,14 +1,7 @@
-resource "aws_instance" "this" {    # ses-34 
+resource "aws_instance" "this" {  
   ami           = var.ami_id  # manadatory
   instance_type = var.instace_moduletype  # manadatory
   vpc_security_group_ids = var.sg_ids # manadatory
   tags = var.tags # Optional
 } 
 
-# so keys ni changes chyelemu so vati ki vunna values change chetse we can get o/p 
-
-# General ga use chese code start using word is "this" syntax not a mandatory just for understand purpose
-# 
-
-
-# test file name is roboshop-ec2-test see there for results
--- variable.tf
+++ variable.tf
@@ -1,31 +1,28 @@
-# ses-34 the below code ++
+
 
 variable "ami_id" { 
-  type = string # list of ami-id lu iste error vastundhi bcz here value only string
-  default = "" # ikkada values ichhina user ki chance vunttadhi for edit... so leave it
-  # so ila empty ga pedithe values update chayli must be provide
+  type = string
+  default = ""  # we have given a chance to user,
   description = "this is the ami used to create EC2 server"
   
 }
 
 variable "instace_moduletype" {     
 type = string
-# so ila empty ga pedithe values update chayli must be provide
 description = "Instance type is used for creating EC2 instance or server"
 validation {
     condition     = contains(["t3.micro", "t3.small", "t3.medium"], var.instace_moduletype)
     error_message = "Please select either t3 micro or small or medium"
   }
-# see in notes
 }
 
 variable "sg_ids" {
   type = list
-  # so ila empty ga pedithe values update chayli must be provide while testing this module or using this module
+  # here we haven't mention any defalut value bcz it is mandatory
 }
 
 # optional
 variable "tags" {
   type = map
-  default = {} # ila pedithe idhi optinal ani meaning9
+  default = {} # it tell us, it is optional
 }
